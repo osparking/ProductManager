fc801409c22b26e874d10e9a7d857d4f7d420167
osparking <jbpark03@gmail.com>
2023-03-20 14:53:59 +0900

    domain class that mapps to db table

---
+++ src/main/java/space/bumtiger/product/Product.java
@@ -0,0 +1,19 @@
+package space.bumtiger.product;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Table
+@NoArgsConstructor
+public class Product {
+	@Id
+  private Long id;
+  private String name;
+  private String brand;
+  private String madein;
+  private float price;
+}
