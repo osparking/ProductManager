d5d65038212922eca0464f5b7ef44b95669396e4
osparking <jbpark03@gmail.com>
2023-03-20 19:59:24 +0900

    product list view handler method

--- src/main/java/space/bumtiger/product/AppController.java
+++ src/main/java/space/bumtiger/product/AppController.java
@@ -1,13 +1,23 @@
 package space.bumtiger.product;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AppController {
 	@Autowired
 	private ProductService service;
 	
-	// 처리기 메소드들...
+	@GetMapping("/")
+	public String homePage(Model model) {
+		List<Product> products = service.listAll();
+		model.addAttribute("products", products);
+		return "index";
+	}
 
 }
