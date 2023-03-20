e2a67044ae27348bee0e2f50a98833c67e00a28c
osparking <jbpark03@gmail.com>
2023-03-20 12:59:30 +0900

    initial project status

---
+++ src/main/java/space/bumtiger/product/ProductManagerApplication.java
@@ -0,0 +1,13 @@
+package space.bumtiger.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductManagerApplication.class, args);
+	}
+
+}
