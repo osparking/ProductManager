6d7106da8f46cb0c0bc1f1c35109d6f14ee7a45d
osparking <jbpark03@gmail.com>
2023-03-21 10:56:40 +0900

    new product save handler - v1

--- src/main/java/space/bumtiger/product/AppController.java
+++ src/main/java/space/bumtiger/product/AppController.java
@@ -6,13 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AppController {
 	@Autowired
 	private ProductService service;
 
+	@PostMapping("/save") 
+	public String saveProduct (Product product){
+		service.save(product);
+		return "redirect:/";
+	}
+	
 	@GetMapping("/new")
 	public String newProductView(Model model) {
 		var product = new Product();
