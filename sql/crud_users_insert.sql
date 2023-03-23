e08be24fb9f419ba087d72ac1e1c4b864371282d
osparking <jbpark03@gmail.com>
2023-03-23 22:52:52 +0900

    insert two users into users table

---
+++ sql/crud_users_insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO `users` (`username`,`password`,`role`,`enabled`)
+VALUES ('namhm',
+'$2a$10$XptfskLsT1l/bRTLRiiCgejHqOpgXFreUnNUa35gJdCr2v2QbVFzu',
+'ROLE_USER', 1);
+ 
+INSERT INTO `users` (`username`,`password`,`role`,`enabled`)
+VALUES ('admin',
+'$2a$10$zxvEq8XzYEYtNjbkRsJEbukHeRx3XS6MDXHMu8cNuNsRfZJWwswDy',
+'ROLE_ADMIN', 1);
