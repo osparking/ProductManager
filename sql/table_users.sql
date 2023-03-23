573b8a5a3cc8bdcb49044c6762e0ed2a949a403b
osparking <jbpark03@gmail.com>
2023-03-23 09:35:34 +0900

    users table schema

---
+++ sql/table_users.sql
@@ -0,0 +1,10 @@
+-- sales.users definition
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `role` varchar(45) NOT NULL,
+  `enabled` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
