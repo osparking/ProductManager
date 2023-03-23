43ae08938053d377a44a23767f461fe1126241cd
osparking <jbpark03@gmail.com>
2023-03-23 13:10:39 +0900

    user repository with one extra method

---
+++ src/main/java/space/bumtiger/product/UserRepository.java
@@ -0,0 +1,10 @@
+package space.bumtiger.product;
+
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+	@Query("SELECT u FROM User u WHERE u.username = :username")
+	public User getUserByUsername(@Param("username") String username);
+}
