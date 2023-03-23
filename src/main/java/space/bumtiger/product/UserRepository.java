fc95a526d7b284a2ebbf65443b8c7fe57a43457f
osparking <jbpark03@gmail.com>
2023-03-24 07:28:23 +0900

    needless query anno removed

--- src/main/java/space/bumtiger/product/UserRepository.java
+++ src/main/java/space/bumtiger/product/UserRepository.java
@@ -1,10 +1,7 @@
 package space.bumtiger.product;
 
-import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends CrudRepository<User, Long> {
-	@Query("SELECT * FROM users u WHERE u.username = :username")
-	public User findByUsername(@Param("username") String username);
+	public User findByUsername(String username);
 }
