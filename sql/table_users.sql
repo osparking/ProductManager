b17ac79aab288b0943347a23c5b375a92fc7d077
osparking <jbpark03@gmail.com>
2023-03-24 22:14:28 +0900

    login credentials provider

--- sql/table_users.sql
+++ sql/table_users.sql
@@ -6,5 +6,6 @@ CREATE TABLE `users` (
   `password` varchar(64) NOT NULL,
   `role` varchar(45) NOT NULL,
   `enabled` tinyint(1) DEFAULT 0,
+  `provider` varchar(15) DEFAULT NULL,
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
