c24f5ef717abb94dac4610006b3160706d7319dc
osparking <jbpark03@gmail.com>
2023-03-27 13:20:17 +0900

    OAuth2User implementation

---
+++ src/main/java/space/bumtiger/product/security/oauth/CustomOAuth2User.java
@@ -0,0 +1,34 @@
+package space.bumtiger.product.security.oauth;
+
+import java.util.Collection;
+import java.util.Map;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class CustomOAuth2User implements OAuth2User {
+
+	private OAuth2User oAuth2User;
+
+	@Override
+	public Map<String, Object> getAttributes() {
+		return oAuth2User.getAttributes();
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return oAuth2User.getAuthorities();
+	}
+
+	@Override
+	public String getName() {
+		return oAuth2User.getName();
+	}
+
+	public String getEmail() {
+		return oAuth2User.<String>getAttribute("email");
+	}
+}
