9b5405c4b4c77b61a88166d8ee79b9e842e50a3b
osparking <jbpark03@gmail.com>
2023-03-29 07:02:59 +0900

    facebook user credentials saved

--- src/main/java/space/bumtiger/product/security/oauth/CustomOAuth2UserService.java
+++ src/main/java/space/bumtiger/product/security/oauth/CustomOAuth2UserService.java
@@ -1,10 +1,15 @@
 package space.bumtiger.product.security.oauth;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+
+import space.bumtiger.product.Provider;
+import space.bumtiger.product.security.User;
+import space.bumtiger.product.security.UserRepository;
 
 @Service
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
@@ -14,4 +19,21 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 		OAuth2User user = super.loadUser(userRequest);
 		return new CustomOAuth2User(user);
 	}
+
+	@Autowired
+	private UserRepository userRepo;
+
+	public void processOAuthPostLogin(String email) {
+		User dbUser = userRepo.findByUsername(email);
+
+		if (dbUser == null) {
+			User newUser = new User();
+			newUser.setUsername(email);
+			newUser.setProvider(Provider.FACEBOOK);
+			newUser.setEnabled(true);
+
+			userRepo.save(newUser);
+		}
+	}
+
 }
