4665a82988d488b6d07d3f9fa723654bbe1ceb43
osparking <jbpark03@gmail.com>
2023-03-28 23:05:58 +0900

    facebook login after handler specified

--- src/main/java/space/bumtiger/product/security/SecurityConfig.java
+++ src/main/java/space/bumtiger/product/security/SecurityConfig.java
@@ -1,14 +1,22 @@
 package space.bumtiger.product.security;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import space.bumtiger.product.security.oauth.CustomOAuth2User;
 import space.bumtiger.product.security.oauth.CustomOAuth2UserService;
 
 @Configuration
@@ -36,7 +44,20 @@ public class SecurityConfig {
 				.oauth2Login(oauth2 -> oauth2
 					.loginPage("/login")
 						.userInfoEndpoint()
-						.userService(oauth2UserService));
+						.userService(oauth2UserService)
+					.and()
+						.successHandler(new AuthenticationSuccessHandler() {
+							@Override
+							public void onAuthenticationSuccess(HttpServletRequest request,
+									HttpServletResponse response, Authentication authentication)
+									throws IOException, ServletException {
+								CustomOAuth2User user = (CustomOAuth2User) authentication
+										.getPrincipal();
+								oauth2UserService.processOAuthPostLogin(user.getEmail());
+								response.sendRedirect("/");
+							}
+						})
+				);
 
 		return http.build();
 	}
